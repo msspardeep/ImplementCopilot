2a0a50f5943016b3be72e2fe05fb15c25d9af861
mss_shubhambatra <shubham.batra@mastersoftwaresolutions.com>
2024-06-03 11:59:36 +0530

    Add project files.

---
+++ ImplementCopilot/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ImplementCopilot.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
